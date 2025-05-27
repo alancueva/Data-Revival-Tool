b52a671e9e8d1d54d4a87da4de7aae401bbc9f7f
alancuevat <alancuevat@gmail.com>
2025-05-27 11:45:15 -0500

    añadio comentario, corigio las direcciones  en FileEngine

--- include/core/FileEngine.h
+++ include/core/FileEngine.h
@@ -9,22 +9,33 @@
 #include <future>
 #include "../metadata/ImageMetadata.h"
 #include "../metadata/DocumentMetadata.h"
+#include "../metadata/AudioMetadata.h"
 
 using namespace std;
 
-class FileEngine {
+/**
+ * * @brief Clase que representa el motor de procesamiento de archivos.
+ */
+class FileEngine
+{
 private:
+    /**
+     * * @brief Punteros únicos para almacenar metadatos de diferentes tipos de archivos.
+     * * @note Utiliza punteros únicos para gestionar la memoria de manera eficiente.
+     */
     unique_ptr<ImageMetadata> imageMetadata;
     unique_ptr<DocumentMetadata> documentMetadata;
+    unique_ptr<AudioMetadata> audioMetadata;
+
     string filePath;
-    string getFileType(const filesystem::path& filePath);
-    string processFile(const string& filePath);
+    string getFileType(const filesystem::path &filePath);
+    string processFile(const string &filePath);
+
 public:
     FileEngine();
     ~FileEngine();
-    
-    future<string> processFileAsync(const string& filePath);
-};
 
+    future<string> processFileAsync(const string &filePath);
+};
 
 #endif
--- src/core/FileEngine.cpp
+++ src/core/FileEngine.cpp
@@ -12,55 +12,80 @@
 
 using namespace std;
 
-FileEngine::FileEngine(){
+FileEngine::FileEngine()
+{
+    /**
+     * @brief Inicialización de los punteros únicos para almacenar metadatos de diferentes 
+     * tipos de archivos.
+     * @note Utiliza punteros únicos para gestionar la memoria de manera eficiente.
+     */
     imageMetadata = make_unique<ImageMetadata>();
     documentMetadata = make_unique<DocumentMetadata>();
+    audioMetadata = make_unique<AudioMetadata>();
     filePath = "";
 }
 
-FileEngine::~FileEngine()=default;
-
+FileEngine::~FileEngine() = default;
 
 /**
- * 
+ *
  * @brief Método para analizar el tipo de archivo.
  * @param filePath Ruta del archivo a analizar.
  * @return string con el tipo de archivo.
  */
-string FileEngine::getFileType(const filesystem::path& filePath) {
+string FileEngine::getFileType(const filesystem::path &filePath)
+{
     string ext = filePath.extension().string();
     transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
 
-    if (ext == ".txt" || ext == ".doc" || ext == ".docx" || ext == ".pdf" || ext == ".xls" || 
-        ext == ".xlsx" || ext == ".ppt" || ext == ".pptx" || ext == ".csv" || ext == ".xml") {
+    if (ext == ".txt" || ext == ".doc" || ext == ".docx" || ext == ".pdf" || ext == ".xls" ||
+        ext == ".xlsx" || ext == ".ppt" || ext == ".pptx" || ext == ".csv" || ext == ".xml")
+    {
         return "Documento";
-    } else if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" || ext == ".bmp") {
+    }
+    else if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" || ext == ".bmp")
+    {
         return "Imagen";
-    } else if (ext == ".mp3" || ext == ".wav" || ext == ".ogg" || ext == ".flac" || ext == ".aac") {
+    }
+    else if (ext == ".mp3" || ext == ".wav" || ext == ".ogg" || ext == ".flac" || ext == ".aac")
+    {
         return "Audio";
-    } else if (ext == ".mp4" || ext == ".avi" || ext == ".mov" || ext == ".mkv" || ext == ".wmv" ||
-         ext == ".flv" || ext == ".webm") {
+    }
+    else if (ext == ".mp4" || ext == ".avi" || ext == ".mov" || ext == ".mkv" || ext == ".wmv" ||
+             ext == ".flv" || ext == ".webm")
+    {
         return "Video";
-    } else if (ext == ".exe" || ext == ".bin" || ext == ".dll" || ext == ".so" || ext == ".app") {
+    }
+    else if (ext == ".exe" || ext == ".bin" || ext == ".dll" || ext == ".so" || ext == ".app")
+    {
         return "Ejecutable";
-    } else if (ext == ".zip" || ext == ".rar" || ext == ".tar" || ext == ".gz") {
+    }
+    else if (ext == ".zip" || ext == ".rar" || ext == ".tar" || ext == ".gz")
+    {
         return "Archivo Comprimido";
-    } else if (ext == ".cpp" || ext == ".h" || ext == ".py" || ext == ".js" || ext == ".java"
-               || ext == ".c" || ext == ".cs" || ext == ".go" || ext == ".rb" || ext == ".php"
-               || ext == ".swift" || ext == ".xml"
-               || ext == ".json" || ext == ".yaml" || ext == ".sql" || ext == ".pl" || ext == ".sh"
-               || ext == ".bash") {
+    }
+    else if (ext == ".cpp" || ext == ".h" || ext == ".py" || ext == ".js" || ext == ".java" || ext == ".c" || ext == ".cs" || ext == ".go" || ext == ".rb" || ext == ".php" || ext == ".swift" || ext == ".xml" || ext == ".json" || ext == ".yaml" || ext == ".sql" || ext == ".pl" || ext == ".sh" || ext == ".bash")
+    {
         return "Código Fuente";
-    } else if (ext == ".html" || ext == ".css" || ext == ".js"
-               || ext == ".php" || ext == ".asp" || ext == ".jsp") {
+    }
+    else if (ext == ".html" || ext == ".css" || ext == ".js" || ext == ".php" || ext == ".asp" || ext == ".jsp")
+    {
         return "Página Web";
-    } else if (ext == ".iso" || ext == ".img" || ext == ".dmg") {
+    }
+    else if (ext == ".iso" || ext == ".img" || ext == ".dmg")
+    {
         return "Imagen de Disco";
-    } else if (ext == ".log") {
+    }
+    else if (ext == ".log")
+    {
         return "Archivo de Registro";
-    } else if (ext == ".json") {
+    }
+    else if (ext == ".json")
+    {
         return "Archivo JSON";
-    } else {
+    }
+    else
+    {
         return "Desconocido";
     }
 }
@@ -70,24 +95,76 @@ string FileEngine::getFileType(const filesystem::path& filePath) {
  * @brief Método para procesar el archivo.
  * @param filePath Ruta del archivo a procesar.
  * @return string con el resultado del procesamiento.
+ * @details Este método analiza el tipo de archivo y extrae los metadatos correspondientes.
+ * * - ifstream: Clase de C++ que permite leer archivos de forma eficiente.
+ * * - stringstream: Clase de C++ que permite construir cadenas de texto de forma eficiente.
+ * * - filesystem::path: Clase de C++ que representa una ruta de archivo en el sistema de archivos.
  */
-string FileEngine::processFile(const string& filePath) {
+string FileEngine::processFile(const string &filePath)
+{
     filesystem::path path(filePath);
     string fileType = getFileType(path);
     ifstream file(filePath, ios::binary);
     stringstream lstdata;
-    if (file.is_open()) {
-        if (fileType == "Imagen") {
+    if (file.is_open())
+    {
+        if (fileType == "Imagen")
+        {
             cout << "Procesando imagen: " << filePath << endl;
             lstdata << imageMetadata->extractMetadataImg(filePath);
-        } else if (fileType == "Documento") {
+        }
+        else if (fileType == "Documento")
+        {
             lstdata << documentMetadata->getAllMetadata(filePath);
         }
-        else {
+        else if (fileType == "Audio")
+        {
+            lstdata << audioMetadata->getAllMetadata(filePath);
+        }
+        else if (fileType == "Video")
+        {
+            lstdata << "Procesamiento de video no implementado." << endl;
+        }
+        else if (fileType == "Ejecutable")
+        {
+            lstdata << "Procesamiento de ejecutables no implementado." << endl;
+        }
+        else if (fileType == "Archivo Comprimido")
+        {
+            lstdata << "Procesamiento de archivos comprimidos no implementado." << endl;
+        }
+        else if (fileType == "Código Fuente")
+        {
+            lstdata << "Procesamiento de código fuente no implementado." << endl;
+        }
+        else if (fileType == "Página Web")
+        {
+            lstdata << "Procesamiento de páginas web no implementado." << endl;
+        }
+        else if (fileType == "Imagen de Disco")
+        {
+            lstdata << "Procesamiento de imágenes de disco no implementado." << endl;
+        }
+        else if (fileType == "Archivo de Registro")
+        {
+            lstdata << "Procesamiento de archivos de registro no implementado." << endl;
+        }
+        else if (fileType == "Archivo JSON")
+        {
+            lstdata << "Procesamiento de archivos JSON no implementado." << endl;
+        }
+        else if (fileType == "Desconocido")
+        {
+            lstdata << "Tipo de archivo desconocido: " << filePath << endl;
+        }
+        else
+        {
             lstdata << "Tipo de archivo no soportado: " << fileType << endl;
         }
         file.close();
-    } else {
+    }
+    else
+    {
         lstdata << "Error abrir archivo: " << filePath << endl;
     }
     return lstdata.str();
@@ -97,22 +174,22 @@ string FileEngine::processFile(const string& filePath) {
  * @brief Método para procesar el archivo de forma asíncrona.
  * @param filePath Ruta del archivo a procesar.
  * @return future<string> con el resultado del procesamiento.
- * 
+ *
  * @details
  * - future<string>: Representa un valor que estará disponible en el futuro. Se utiliza para operaciones asíncronas
  *   donde el resultado no está disponible inmediatamente sino que se calculará más tarde.
- * 
+ *
  * - async: Una plantilla de función que ejecuta una función de forma asíncrona (potencialmente en un hilo separado),
  *   y devuelve un future que eventualmente contendrá el resultado de esa función.
- * 
+ *
  * - async(launch::async, [this, filePath]()):
  *   - launch::async fuerza la operación a ejecutarse en un nuevo hilo
  *   - [this, filePath]() es una función lambda que captura:
  *     - puntero 'this' para acceder a los miembros de la clase
  *     - parámetro filePath para pasar a processFile
  */
-future<string> FileEngine::processFileAsync(const string& filePath) {
-    return async(launch::async, [this, filePath]() {
-        return this->processFile(filePath);
-    });
+future<string> FileEngine::processFileAsync(const string &filePath)
+{
+    return async(launch::async, [this, filePath]()
+                 { return this->processFile(filePath); });
 }
